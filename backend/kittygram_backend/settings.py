127a06fb319543dcb18431f2241f49582ab9ede7
RedC0mrade <e2e4e2e4@mail.ru>
2023-08-25 12:44:44 +0000

    39

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='wioeofdjw123123')
-DEBUG = False
+DEBUG = os.getenv('DEBUG', default='False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='localhost 127.0.0.1').split()
 
